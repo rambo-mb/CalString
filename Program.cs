84a3a9561b76aafc0d40956726f6385d3515feb4
Izzatjon Qodirov <qodirov.izzatjon.rambo.mb@gmail.com>
2026-05-03 12:28:46 +0500

    ver: with 2 multi digit numbers

--- Program.cs
+++ Program.cs
@@ -1,42 +1,45 @@
-﻿// v3
-// 1+2*3
-// addition, subtraction, multiplication and division with all numbers are single digit number
+﻿// v4
+// 12*45
+// addition, subtraction, multiplication and division with 2 numbers are multi digit number
 
-Console.Write("Ifodani kiriting: ");
+Console.Write("Ifodani kiritng: ");
 string expression = Console.ReadLine();
 
-int sum = 0;
-int lastNumber = expression[0] - '0';
+int index = 0;
 
-for(int index = 1; index < expression.Length; index += 2)
+while (index < expression.Length)
 {
-	switch(expression[index])
+	char operationStr = expression[index];
+	switch (operationStr)
 	{
 		case '+':
 			{
-				sum += lastNumber;
-				lastNumber = expression[index+1] - '0';
+				decimal firstNumber = Convert.ToDecimal(expression.Substring(0, index));
+				decimal secondNumber = Convert.ToDecimal(expression.Substring(index+1));
+				Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
 				break;
 			}
 		case '-':
 			{
-				sum += lastNumber;
-				lastNumber = -(expression[index+1] - '0');
-				break;
-			}
-		case '*':
-			{
-				lastNumber *= expression[index+1] - '0';
+				decimal firstNumber = Convert.ToDecimal(expression.Substring(0, index));
+				decimal secondNumber = Convert.ToDecimal(expression.Substring(index+1));
+				Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
 				break;
 			}
 		case '/':
 			{
-				lastNumber /= expression[index+1] - '0';
+				decimal firstNumber = Convert.ToDecimal(expression.Substring(0, index));
+				decimal secondNumber = Convert.ToDecimal(expression.Substring(index+1));
+				Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+				break;
+			}
+		case '*':
+			{
+				decimal firstNumber = Convert.ToDecimal(expression.Substring(0, index));
+				decimal secondNumber = Convert.ToDecimal(expression.Substring(index+1));
+				Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
 				break;
 			}
 	}
+	index++;
 }
-
-sum += lastNumber;
-
-Console.WriteLine($"{expression}={sum}");

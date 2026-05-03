27168cf533e15a8d5120dc13328c9b64eba79b83
Izzatjon Qodirov <qodirov.izzatjon.rambo.mb@gmail.com>
2026-05-03 10:36:39 +0500

    ver: addition only single digit numbers

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// v1
+// 1+2+3
+// only addition and all numbers are single digit number
+
+Console.Write("Ifodani kiriting: ");
+string expression = Console.ReadLine();
+
+int sum = 0;
+int index = 0;
+
+while(index < expression.Length)
+{
+	sum += Convert.ToInt32(expression[index].ToString());
+	index += 2;
+}
+
+Console.WriteLine($"{expression}={sum}");

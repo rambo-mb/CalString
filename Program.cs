a4bc26a7a18376afced5d9f8c03a2108755eea46
Izzatjon Qodirov <qodirov.izzatjon.rambo.mb@gmail.com>
2026-05-05 16:54:01 +0500

    merge: all versions into single console menu

--- Program.cs
+++ Program.cs
@@ -1,24 +1,196 @@
-﻿// v5
-// 12*45/2+23
-// addition, subtraction, multiplication and division with multi numbers are multi digit number
-
-Console.Write("Ifodani kiriting: ");
-string expression = Console.ReadLine();
-
-int sum = 0;
-int lastNumber = 0;
-int currentNumber = 0;
+﻿string expression = string.Empty;
+decimal sum = 0;
+int index = 0;
+decimal lastNumber = 0;
+decimal currentNumber = 0;
 char operation = '+';
 
-for(int index = 0; index < expression.Length; index++)
+while(true)
 {
-	switch(expression[index])
+  Console.WriteLine("\n╔══════════════════════════════╗");
+  Console.WriteLine("║          CalString           ║");
+  Console.WriteLine("╠══════════════════════════════╣");
+  Console.WriteLine("║ 1. V1 │ Faqat qo'shish       ║");
+  Console.WriteLine("║ 2. V2 │ +  va  -             ║");
+  Console.WriteLine("║ 3. V3 │ + - * /              ║");
+  Console.WriteLine("║ 4. V4 │ Ko'p xonali (2 son)  ║");
+  Console.WriteLine("║ 5. V5 │ Ko'p xonali (to'liq) ║");
+  Console.WriteLine("║ 0.    │ Chiqish              ║");
+  Console.WriteLine("╚══════════════════════════════╝");
+
+  Console.Write("Tanlang: ");
+  string? option = Console.ReadLine();
+
+	if(option == "1" || option == "2" || option == "3" || option == "4" || option == "5")
 	{
-		case '+':
-		case '-':
-		case '*':
-		case '/':
-			{
+		Console.Write("Ifodani kiriting: ");
+		expression = Console.ReadLine()!;
+	}
+
+  switch(option)
+  {
+    case "0":
+		case "":
+      {
+        return;
+      }
+    case "1":
+      {
+				while(index < expression.Length)
+				{
+					sum += Convert.ToInt32(expression[index].ToString());
+					index += 2;
+				}
+
+        break;
+      }
+    case "2":
+      {
+				while(index < expression.Length)
+				{
+					if(index == 0)
+					{
+						sum += Convert.ToInt32(expression[index].ToString());
+					}
+					else if (expression[index-1] == '+'){
+						sum += Convert.ToInt32(expression[index].ToString());
+					}
+					else if (expression[index-1] == '-')
+					{
+						sum -= Convert.ToInt32(expression[index].ToString());
+					}
+					
+					index += 2;
+				}
+
+        break;
+      }
+    case "3":
+      {
+				lastNumber = expression[0] - '0';
+
+				for(index = 1; index < expression.Length; index += 2)
+				{
+					switch(expression[index])
+					{
+						case '+':
+							{
+								sum += lastNumber;
+								lastNumber = expression[index+1] - '0';
+								break;
+							}
+						case '-':
+							{
+								sum += lastNumber;
+								lastNumber = -(expression[index+1] - '0');
+								break;
+							}
+						case '*':
+							{
+								lastNumber *= expression[index+1] - '0';
+								break;
+							}
+						case '/':
+							{
+								lastNumber /= expression[index+1] - '0';
+								break;
+							}
+					}
+				}
+
+				sum += lastNumber;
+
+        break;
+      }
+    case "4":
+      {
+				while (index < expression.Length)
+				{
+					char operationStr = expression[index];
+					switch (operationStr)
+					{
+						case '+':
+							{
+								decimal firstNumber = Convert.ToDecimal(expression.Substring(0, index));
+								decimal secondNumber = Convert.ToDecimal(expression.Substring(index+1));
+								sum = firstNumber + secondNumber;
+								break;
+							}
+						case '-':
+							{
+								decimal firstNumber = Convert.ToDecimal(expression.Substring(0, index));
+								decimal secondNumber = Convert.ToDecimal(expression.Substring(index+1));
+								sum = firstNumber - secondNumber;
+								break;
+							}
+						case '/':
+							{
+								decimal firstNumber = Convert.ToDecimal(expression.Substring(0, index));
+								decimal secondNumber = Convert.ToDecimal(expression.Substring(index+1));
+								sum = firstNumber / secondNumber;
+								break;
+							}
+						case '*':
+							{
+								decimal firstNumber = Convert.ToDecimal(expression.Substring(0, index));
+								decimal secondNumber = Convert.ToDecimal(expression.Substring(index+1));
+								sum = firstNumber * secondNumber;
+								break;
+							}
+					}
+					index++;
+				}
+        break;
+      }
+    case "5":
+      {
+				for(index = 0; index < expression.Length; index++)
+				{
+					switch(expression[index])
+					{
+						case '+':
+						case '-':
+						case '*':
+						case '/':
+							{
+								switch (operation)
+								{
+									case '+':
+										{
+											sum += lastNumber;
+											lastNumber = currentNumber;
+											break;
+										}
+									case '-':
+										{
+											sum += lastNumber;
+											lastNumber = -currentNumber;
+											break;
+										}
+									case '*':
+										{
+											lastNumber *= currentNumber;
+											break;
+										}
+									case '/':
+										{
+											lastNumber /= currentNumber;
+											break;
+										}
+								}
+
+								operation = expression[index];
+								currentNumber = 0;
+								break;
+							}
+						default:
+							{
+								currentNumber = currentNumber * 10 + (expression[index] - '0');
+								break;
+							}
+					}
+				}
+
 				switch (operation)
 				{
 					case '+':
@@ -45,44 +217,21 @@ for(int index = 0; index < expression.Length; index++)
 						}
 				}
 
-				operation = expression[index];
-				currentNumber = 0;
-				break;
-			}
-		default:
-			{
-				currentNumber = currentNumber * 10 + (expression[index] - '0');
-				break;
-			}
-	}
+				sum += lastNumber;
+
+        break;
+      }
+  }
+
+	Console.WriteLine($"{expression}={sum}\n");
+
+	expression = string.Empty;
+	sum = 0;
+	index = 0;
+	lastNumber = 0;
+	currentNumber = 0;
+	operation = '+';
+
+	Console.Write("Davom etasizmi? ");
+	if (!(Console.ReadLine()?.ToLower() == "ha")) return;
 }
-
-switch (operation)
-{
-	case '+':
-		{
-			sum += lastNumber;
-			lastNumber = currentNumber;
-			break;
-		}
-	case '-':
-		{
-			sum += lastNumber;
-			lastNumber = -currentNumber;
-			break;
-		}
-	case '*':
-		{
-			lastNumber *= currentNumber;
-			break;
-		}
-	case '/':
-		{
-			lastNumber /= currentNumber;
-			break;
-		}
-}
-
-sum += lastNumber;
-
-Console.WriteLine($"{expression}={sum}");
